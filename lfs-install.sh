93558a79f77a6a1009251db51400d2b2f93ca0e9
everyonecancode <everyonecancode@gmail.com>
2019-08-02 20:20:34 +0200

    Fixed lfs user settings

--- lfs-install.sh
+++ lfs-install.sh
@@ -30,4 +30,21 @@ useradd -s /bin/bash -g lfs -m -k /dev/null lfs
 chown -v lfs $LFS/tools
 chown -v lfs $LFS/sources
 
-su -c ./build-scripts.sh - lfs
+# Prepare bash script for LFS user
+cat > /home/lfs/.bash_profile << "EOF"
+exec env -i HOME=$HOME TERM=$TERM PS1='\u:\w\$ ' /bin/bash
+EOF
+
+cat > /home/lfs/.bashrc << "EOF"
+set +h
+umask 022
+LFS=/mnt/lfs
+LC_ALL=POSIX
+LFS_TGT=$(uname -m)-lfs-linux-gnu
+PATH=/tools/bin:/bin:/usr/bin
+export LFS LC_ALL LFS_TGT PATH
+EOF
+chown -v lfs /home/lfs/.bash_profile
+chown -v lfs /home/lfs/.bashrc
+
+LFS=$LFS su -c ./build-scripts.sh lfs
