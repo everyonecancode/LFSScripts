f18df88967d6bf3c69addcc0543e2f62fbe95ced
Krygier Piotr <everyonecancode@gmail.com>
2019-08-02 14:13:54 +0200

    Fix wget

--- lfs-install.sh
+++ lfs-install.sh
@@ -18,7 +18,8 @@ set -e
 
 # Get sources and prepare LFS specific directories
 mkdir -v $LFS/sources
-wget --input-file=http://www.linuxfromscratch.org/lfs/view/stable/wget-list --continue --directory-prefix=$LFS/sources
+wget http://www.linuxfromscratch.org/lfs/view/stable/wget-list
+wget --input-file=wget-list --continue --directory-prefix=$LFS/sources
 chmod -v a+wt $LFS/sources
 mkdir -v $LFS/tools
 ln -sv $LFS/tools /
