b7ae39e2e64b966136fb01202b9725c02d17f2f3
Krygier Piotr <everyonecancode@gmail.com>
2019-08-02 15:00:43 +0200

    Trying to run same script as different user

--- lfs-install.sh
+++ lfs-install.sh
@@ -30,7 +30,7 @@ useradd -s /bin/bash -g lfs -m -k /dev/null lfs
 chown -v lfs $LFS/tools
 chown -v lfs $LFS/sources
 
-sudo -i -u lfs bash << eof
+exec sudo -i -u lfs bash - << eof
 
 # Prepare bash script for LFS user
 cat > ~/.bash_profile << "EOF"
