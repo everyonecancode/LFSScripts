fe69f09ac51559cb71cc5036f3e3e9d9e4f2470b
everyonecancode <everyonecancode@gmail.com>
2019-08-02 22:26:03 +0200

    Fix linux header installation

--- build-scripts.sh
+++ build-scripts.sh
@@ -89,7 +89,6 @@ function gcc-pass1()
 #  Linux-4.20.12 API Headers
 function linux_headers()
 {
-  pushd linux-4.20.12
   make mrproper
   make INSTALL_HDR_PATH=dest headers_install
   cp -rv dest/include/* /tools/include
