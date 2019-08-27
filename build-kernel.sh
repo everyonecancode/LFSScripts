72df6014e7c09c46506fd4a8dee3931bc35f58ea
everyonecancode <everyonecancode@gmail.com>
2019-08-27 22:56:07 +0200

    Fix kernel build

--- build-kernel.sh
+++ build-kernel.sh
@@ -5,17 +5,20 @@ pushd /sources
 tar -xf linux-4.20.12.tar.xz
 pushd linux-4.20.12
 
+mkdir -pv /usr/pkg/linux/4.20.12/usr/share/doc/linux-4.20.12/
 make mrproper
 make defconfig
 make
 make modules_install
-cp -iv arch/x86/boot/bzImage /boot/vmlinuz-4.20.12-lfs-8.4
-cp -iv System.map /boot/System.map-4.20.12
-cp -iv .config /boot/config-4.20.12
+cp -fv arch/x86/boot/bzImage /boot/vmlinuz-4.20.12-lfs-8.4
+cp -fv System.map /boot/System.map-4.20.12
+cp -fv .config /boot/config-4.20.12
 install -d /usr/pkg/linux/4.20.12/share/doc/linux-4.20.12
 cp -rv Documentation/*  /usr/pkg/linux/4.20.12/usr/share/doc/linux-4.20.12
-mkdir -pv usr/share/doc/linux-4.20.12/
+
+# Create links for documentation
 cp -rvs /usr/pkg/linux/4.20.12/usr/share/doc/linux-4.20.12/* usr/share/doc/linux-4.20.12/
+
 
 # Create modprobe config file
 install -v -m755 -d /etc/modprobe.d
